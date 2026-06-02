2bf7156f30ff1cc4aac2777526f0b5b46365690d
bbs-sk <skripsibbs@gmail.com>
2026-06-02 12:35:18 +0700

    John-Jam

--- src/controllers/mix.js
+++ src/controllers/mix.js
@@ -5,7 +5,10 @@ export async function getActivity(req, res) {
     const result = await pool.query(`
       SELECT 
           b.nama_barang,
-          bm.datetime AS datetime,
+          TO_CHAR(
+            bm.datetime,
+            'YYYY-MM-DD HH24:MI:SS'
+          ) AS datetime,
           bm.jumlah,
           bm.harga_beli AS harga,
           b.satuan,
@@ -19,7 +22,10 @@ export async function getActivity(req, res) {
 
       SELECT 
           b.nama_barang,
-          bk.datetime AS datetime,
+          TO_CHAR(
+            bk.datetime,
+            'YYYY-MM-DD HH24:MI:SS'
+          ) AS datetime,
           bk.jumlah,
           bk.harga_jual AS harga,
           b.satuan,
@@ -33,7 +39,10 @@ export async function getActivity(req, res) {
 
       SELECT 
           b.nama_barang,
-          r.datetime AS datetime,
+          TO_CHAR(
+            r.datetime,
+            'YYYY-MM-DD HH24:MI:SS'
+          ) AS datetime,
           r.jumlah,
           r.harga_jual AS harga,
           b.satuan,
